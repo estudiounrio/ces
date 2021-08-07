7dffb8a2144683a1c13aa04de340dc0577e0c0e7
estudiounrio <fmartegani.mp@gmail.com>
2021-08-07 15:24:37 -0300

    SVG

--- dataProcess/sliders.js
+++ dataProcess/sliders.js
@@ -1,13 +1,13 @@
 const diapo = (lado,cantidad) => {
     let diapositivas = `
         <div class="carousel-item active">
-            <img src="img/sliders/${lado}/Diapositiva1.svg" class="d-block w-100" alt="...">
+            <img src="img/sliders/${lado}/Diapositiva1.SVG" class="d-block w-100" alt="...">
         </div>
     `
     for(let i=2;i<=cantidad;i++){
         diapositivas += `
         <div class="carousel-item">
-            <img src="img/sliders/${lado}/Diapositiva${i}.svg" class="d-block w-100" alt="...">
+            <img src="img/sliders/${lado}/Diapositiva${i}.SVG" class="d-block w-100" alt="...">
         </div>
         `
     }

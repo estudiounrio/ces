41f661d67d3ed79e3913497cbf3e71e906671435
estudiounrio <fmartegani.mp@gmail.com>
2021-08-10 22:02:02 -0300

    sin sliders

--- dataProcess/sliders.js
+++ dataProcess/sliders.js
@@ -15,20 +15,20 @@ const diapo = (lado,cantidad) => {
 }
 
 
-document.getElementById('diapositivasDerechaCoyuntural').innerHTML = diapo('coyuntural_derecha',5)
-document.getElementById('diapositivasDerechaCoyuntural_abajo').innerHTML = diapo('coyuntural_derecha-abajo',4)
-document.getElementById('diapositivasIzquierdaCoyuntural').innerHTML = diapo('coyuntural_izquierda',3)
-document.getElementById('diapositivasIzquierdaCoyuntural_abajo').innerHTML = diapo('coyuntural_izquierda-abajo',4)
+// document.getElementById('diapositivasDerechaCoyuntural').innerHTML = diapo('coyuntural_derecha',5)
+// document.getElementById('diapositivasDerechaCoyuntural_abajo').innerHTML = diapo('coyuntural_derecha-abajo',4)
+// document.getElementById('diapositivasIzquierdaCoyuntural').innerHTML = diapo('coyuntural_izquierda',3)
+// document.getElementById('diapositivasIzquierdaCoyuntural_abajo').innerHTML = diapo('coyuntural_izquierda-abajo',4)
 
 
-document.getElementById('diapositivasIzquierda').innerHTML = diapo('izquierda',4)
-document.getElementById('diapositivasDerecha').innerHTML = diapo('derecha',6)
-document.getElementById('diapositivasDerecha_abajo').innerHTML = diapo('derecha_abajo',5)
-document.getElementById('diapositivasIzquierda_abajo').innerHTML = diapo('izquierda_abajo',4)
+// document.getElementById('diapositivasIzquierda').innerHTML = diapo('izquierda',4)
+// document.getElementById('diapositivasDerecha').innerHTML = diapo('derecha',6)
+// document.getElementById('diapositivasDerecha_abajo').innerHTML = diapo('derecha_abajo',5)
+// document.getElementById('diapositivasIzquierda_abajo').innerHTML = diapo('izquierda_abajo',4)
 
 
 
 
-document.getElementById('cerrar').addEventListener('click', e =>
-    document.getElementById('slider').innerHTML = ''
-)
+// document.getElementById('cerrar').addEventListener('click', e =>
+//     document.getElementById('slider').innerHTML = ''
+// )

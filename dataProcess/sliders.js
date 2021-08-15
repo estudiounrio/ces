4f8996cf9fe4360f96b4b02661a6404b48daa302
estudiounrio <fmartegani.mp@gmail.com>
2021-08-14 23:20:05 -0300

    nuevos sliders

--- dataProcess/sliders.js
+++ dataProcess/sliders.js
@@ -1,32 +1,43 @@
-const diapo = (lado,cantidad) => {
-    let diapositivas = `
-        <div class="carousel-item active">
-            <img src="img/sliders/${lado}/Diapositiva1.png" class="d-block w-100" alt="...">
-        </div>
+
+const diapo = (lado,cantidad,nro) => {
+
+    let carrousel = `
+        <div id="carouselExampleIndicators${nro}" class="carousel" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/sliders/${lado}/Diapositiva1.PNG" class="d-block w-100" alt="...">
+                </div>
     `
-    for(let i=2;i<=cantidad;i++){
-        diapositivas += `
-        <div class="carousel-item">
-            <img src="img/sliders/${lado}/Diapositiva${i}.png" class="d-block w-100" alt="...">
+            for(let i=2;i<=cantidad;i++){
+                carrousel += `
+                <div class="carousel-item">
+                    <img src="img/sliders/${lado}/Diapositiva${i}.PNG" class="d-block w-100" alt="...">
+                </div>
+                    `
+            }
+
+        carrousel+= `
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators${nro}" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators${nro}" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+            </button>
         </div>
         `
-    }
-    return diapositivas
+    return carrousel
+    
 }
 
 
-document.getElementById('diapositivasDerechaCoyuntural').innerHTML = diapo('coyuntural_derecha',5)
-// document.getElementById('diapositivasDerechaCoyuntural_abajo').innerHTML = diapo('coyuntural_derecha-abajo',4)
-document.getElementById('diapositivasIzquierdaCoyuntural').innerHTML = diapo('coyuntural_izquierda',3)
-// document.getElementById('diapositivasIzquierdaCoyuntural_abajo').innerHTML = diapo('coyuntural_izquierda-abajo',4)
 
-
-// document.getElementById('diapositivasIzquierda').innerHTML = diapo('izquierda',4)
-// document.getElementById('diapositivasDerecha').innerHTML = diapo('derecha',6)
-// document.getElementById('diapositivasDerecha_abajo').innerHTML = diapo('derecha_abajo',5)
-// document.getElementById('diapositivasIzquierda_abajo').innerHTML = diapo('izquierda_abajo',4)
-
-
+document.getElementById('ipi').innerHTML = diapo('ipi',5,1)
+document.getElementById('emae').innerHTML = diapo('emae',3,2)
+document.getElementById('uci').innerHTML = diapo('uci',4,3)
+document.getElementById('sipim').innerHTML = diapo('sipim',4,4)
 
 
 document.getElementById('cerrar').addEventListener('click', e =>
